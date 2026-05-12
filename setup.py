297a370e6be0535f8cabbf08d5068df588ebfa4f
harshav6 <harshavardhanv16@gmail.com>
2026-05-12 10:31:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Harshavardhan",
+    author_email="harshavardhanv16@gmail.com",
+
+    packages=find_packages(),
+    install_requires=[]
+)
